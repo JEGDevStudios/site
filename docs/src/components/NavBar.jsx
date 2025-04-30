94001c8219b535abc1164131d90164f83661b949
Jhonatan Espinal <153306997+JhonEG-17@users.noreply.github.com>
2025-04-30 10:56:42 -0600

    refactor: correction urls and change in routes files, add files SEO

--- docs/src/components/NavBar.jsx
+++ docs/src/components/NavBar.jsx
@@ -4,15 +4,15 @@ import iconLogo from '../assets/img/logo/logoredondogris.webp';
 function NavBar() {
     return (
         <header>
-            <nav className="navbar navbar-expand-lg bg-black fixed-top w-100">
-                <div className="container-fluid">
+            <nav className="navbar navbar-expand-lg align-items-center bg-black fixed-top w-100">
+                <div className="container-fluid align-items-center">
                     {/* Logo y Nombre */}
-                    <Link to="/" aria-current="page" className="navbar-brand text-light">
+                    <Link to="/" aria-current="page" className="navbar-brand text-light align-items-center text-center p-0">
                         <img
                             src={iconLogo}
                             alt="Logo"
-                            width={30}
-                            height={24}
+                            width={40}
+                            height={40}
                             className="d-inline-block align-text-top p-1"
                         />
                         JEG Dev Studios
@@ -34,7 +34,7 @@ function NavBar() {
                     {/* Menú */}
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav ms-auto"> {/* Clave: ms-auto alinea a la derecha */}
-                            <li className="nav-item">
+                            <li className="nav-item ">
                                 <Link to="/" className="nav-link active text-light" aria-current="page">
                                     Home
                                 </Link>
--- docs/src/main.jsx
+++ docs/src/main.jsx
@@ -1,12 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+const redirect = sessionStorage.redirect;
+if (redirect) {
+  delete sessionStorage.redirect;
+  window.history.replaceState(null, '', redirect);
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
