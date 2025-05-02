3f872bc5960078e4c07326a94c1761908f0be380
Jhonatan Espinal <153306997+JhonEG-17@users.noreply.github.com>
2025-05-02 16:05:57 -0600

    change in lib boostrap files

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
 import BootstrapClient from '../components/BootstrapClient';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
--- src/components/BootstrapClient.js
+++ src/components/BootstrapClient.js
@@ -4,11 +4,10 @@ import { useEffect } from 'react';
 
 function BootstrapClient() {
   useEffect(() => {
-    // Importa el bundle JS de Bootstrap que incluye Popper
-    require('bootstrap/dist/js/bootstrap.bundle.min.js');
+    require('../bootstrap/js/bootstrap.bundle.min.js');
   }, []);
 
-  return null; // Este componente no necesita renderizar nada visualmente
+  return null;
 }
 
 export default BootstrapClient;
