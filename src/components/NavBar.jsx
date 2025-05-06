0f0b67bc715af2b8f759671ad281db3f08609a89
Jhonatan Espinal <153306997+JhonEG-17@users.noreply.github.com>
2025-05-06 17:36:34 -0600

    refactor: change in design for first home section

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,10 +4,10 @@ import iconLogo from '../assets/img/logo/logoredondogris.webp';
 function NavBar() {
     return (
         <header>
-            <nav className="navbar navbar-expand-lg align-items-center bg-black fixed-top w-100">
+            <nav className="navbar navbar-expand-lg align-items-center bg-white fixed-top w-100">
                 <div className="container-fluid d-flex align-items-center">
                     {/* Logo y Nombre */}
-                    <Link to="/" aria-current="page" className="navbar-brand text-light d-flex justify-content-center align-items-center text-center p-0">
+                    <Link to="/" aria-current="page" className="navbar-brand d-flex text-black justify-content-center align-items-center text-center p-0">
                         <img
                             src={iconLogo}
                             alt="Logo"
@@ -21,7 +21,7 @@ function NavBar() {
 
                     {/* Botón Hamburguesa para móviles */}
                     <button
-                        className="navbar-toggler bg-light"
+                        className="navbar-toggler bg-white border-0"
                         type="button"
                         data-bs-toggle="collapse"
                         data-bs-target="#navbarNav"
@@ -36,32 +36,32 @@ function NavBar() {
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav ms-auto"> {/* Clave: ms-auto alinea a la derecha */}
                             <li className="nav-item ">
-                                <Link to="/" className="nav-link active text-light" aria-current="page">
+                                <Link to="/" className="nav-link active text-black" aria-current="page">
                                     Home
                                 </Link>
                             </li>
                             <li className="nav-item">
-                                <Link to="/services" className="nav-link active text-light">
+                                <Link to="/services" className="nav-link active text-black">
                                     Servicios
                                 </Link>
                             </li>
                             <li className="nav-item">
-                                <Link to="/proyects" className="nav-link disabled text-light" style={{ display: 'none' }}>
+                                <Link to="/proyects" className="nav-link disabled text-black" style={{ display: 'none' }}>
                                     Proyectos
                                 </Link>
                             </li>
                             <li className="nav-item">
-                                <Link to="/about" className="nav-link active text-light">
+                                <Link to="/about" className="nav-link active text-black">
                                     Sobre Nosotros
                                 </Link>
                             </li>
                             <li className="nav-item" style={{ display: 'none' }}>
-                                <Link to="/blog" className="nav-link disabled text-light">
+                                <Link to="/blog" className="nav-link disabled text-black">
                                     Blog
                                 </Link>
                             </li>
                             <li className="nav-item">
-                                <Link to="/contact" className="nav-link active text-light">
+                                <Link to="/contact" className="nav-link active text-black">
                                     Contacto
                                 </Link>
                             </li>
